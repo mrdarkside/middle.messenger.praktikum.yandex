897cd9cccf247c5216b4fff55ac66b071c1a7814
MrDarkside <tomitomion@gmail.com>
2023-03-05 04:22:36 +0300

    :sparkles: add block class

---
+++ src/utils/Block.ts
@@ -0,0 +1,212 @@
+import { TemplateDelegate } from 'handlebars';
+import { nanoid } from 'nanoid';
+import EventBus from './EventBus';
+
+export default class Block<P extends Record<string, any> = any> {
+  static EVENTS = {
+    INIT: 'init',
+    FLOW_CDM: 'flow:component-did-mount',
+    FLOW_CDU: 'flow:component-did-update',
+    FLOW_RENDER: 'flow:render',
+  };
+
+  public id = nanoid(6);
+  private eventBus: () => EventBus;
+  protected props: P;
+  public children: Record<string, Block | Block[]>;
+  private _element: HTMLElement | null = null;
+
+  constructor(propsWithChildren: P) {
+    const eventBus = new EventBus();
+
+    const { props, children } = this._getChildrenAndProps(propsWithChildren);
+
+    this.props = this._makePropsProxy(props);
+    this.children = children;
+
+    this.eventBus = () => eventBus;
+
+    this._registerEvents(eventBus);
+    eventBus.emit(Block.EVENTS.INIT);
+  }
+
+  private _getChildrenAndProps(childrenAndProps: P): {
+    props: P;
+    children: Record<string, Block | Block[]>;
+  } {
+    const props: Record<string, unknown> = {};
+    const children: Record<string, Block | Block[]> = {};
+
+    Object.entries(childrenAndProps).forEach(([key, value]) => {
+      if (
+        Array.isArray(value) &&
+        value.length > 0 &&
+        value.every((v) => v instanceof Block)
+      ) {
+        children[key as string] = value;
+      } else if (value instanceof Block) {
+        children[key as string] = value;
+      } else {
+        props[key] = value;
+      }
+    });
+
+    return { props: props as P, children };
+  }
+
+  _addEvents() {
+    const { events = {} } = this.props as P & {
+      events: Record<string, () => void>;
+    };
+
+    Object.keys(events).forEach((eventName) => {
+      this._element?.addEventListener(eventName, events[eventName]);
+    });
+  }
+
+  private _registerEvents(eventBus: EventBus) {
+    eventBus.on(Block.EVENTS.INIT, this._init.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_CDU, this._componentDidUpdate.bind(this));
+    eventBus.on(Block.EVENTS.FLOW_RENDER, this._render.bind(this));
+  }
+
+  private _init() {
+    this.init();
+
+    console.log('block.children', this.children, this.id);
+
+    this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+  }
+
+  protected init() {}
+
+  private _componentDidMount() {
+    this.componentDidMount();
+  }
+
+  componentDidMount() {}
+
+  public dispatchComponentDidMount() {
+    this.eventBus().emit(Block.EVENTS.FLOW_CDM);
+
+    Object.values(this.children).forEach((child) => {
+      if (Array.isArray(child)) {
+        child.forEach((ch) => ch.dispatchComponentDidMount());
+      } else {
+        child.dispatchComponentDidMount();
+      }
+    });
+  }
+
+  private _componentDidUpdate(oldProps: P, newProps: P) {
+    if (this.componentDidUpdate(oldProps, newProps)) {
+      this.eventBus().emit(Block.EVENTS.FLOW_RENDER);
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  protected componentDidUpdate(oldProps: P, newProps: P) {
+    return true;
+  }
+
+  setProps = (nextProps: Partial<P>) => {
+    if (!nextProps) {
+      return;
+    }
+
+    Object.assign(this.props, nextProps);
+  };
+
+  get element() {
+    return this._element;
+  }
+
+  private _render() {
+    const fragment = this.render();
+
+    const newElement = fragment.firstElementChild as HTMLElement;
+
+    if (this._element && newElement) {
+      this._element.replaceWith(newElement);
+    }
+
+    this._element = newElement;
+
+    this._addEvents();
+  }
+
+  protected render(): DocumentFragment {
+    return new DocumentFragment();
+  }
+
+  protected compile(template: TemplateDelegate, context: any) {
+    const contextAndStubs = { ...context };
+
+    Object.entries(this.children).forEach(([name, component]) => {
+      if (Array.isArray(component)) {
+        contextAndStubs[name] = component.map(
+          (child) => `<div data-id="${child.id}"></div>`,
+        );
+      } else {
+        contextAndStubs[name] = `<div data-id="${component.id}"></div>`;
+      }
+    });
+
+    const html = template(contextAndStubs);
+
+    const temp = document.createElement('template');
+
+    temp.innerHTML = html;
+
+    const replaceStub = (component: Block) => {
+      const stub = temp.content.querySelector(`[data-id="${component.id}"]`);
+
+      if (!stub) {
+        return;
+      }
+
+      component.getContent()?.append(...Array.from(stub.childNodes));
+
+      stub.replaceWith(component.getContent()!);
+    };
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    Object.entries(this.children).forEach(([_, component]) => {
+      if (Array.isArray(component)) {
+        component.forEach(replaceStub);
+      } else {
+        replaceStub(component);
+      }
+    });
+
+    return temp.content;
+  }
+
+  getContent() {
+    return this.element;
+  }
+
+  _makePropsProxy(props: P) {
+    const self = this;
+
+    return new Proxy(props, {
+      get(target: P, prop) {
+        const value = target[prop as keyof P];
+        return typeof value === 'function' ? value.bind(target) : value;
+      },
+      set(target, prop, value) {
+        const oldTarget = { ...target };
+
+        const newTarget = { ...target };
+        newTarget[prop as keyof P] = value;
+
+        self.eventBus().emit(Block.EVENTS.FLOW_CDU, oldTarget, newTarget);
+        return true;
+      },
+      deleteProperty() {
+        throw new Error('Нет прав');
+      },
+    });
+  }
+}

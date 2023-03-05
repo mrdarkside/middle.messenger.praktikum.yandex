e532de5724217ebe4209799ee79ab24e0a54ee91
MrDarkside <tomitomion@gmail.com>
2023-03-05 08:08:27 +0300

    :recycle: rebuild 404

---
+++ src/components/error/index.ts
@@ -0,0 +1,18 @@
+import Block from '../../utils/Block';
+import template from './error.hbs';
+import styles from './error.module.scss';
+
+interface ErrorProps {
+  code: number;
+  message: string;
+}
+
+export default class Error extends Block<ErrorProps> {
+  constructor(props: ErrorProps) {
+    super({ ...props });
+  }
+
+  render() {
+    return this.compile(template, { ...this.props, styles });
+  }
+}

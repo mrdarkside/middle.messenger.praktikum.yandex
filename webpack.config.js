42668b02489bc7df1566b23c4fd17d840295d1b0
MrDarkside <tomitomion@gmail.com>
2023-06-10 16:53:45 +0300

    :wrench: webpack settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,80 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-const stylesHandler = 'style-loader';
-
-const config = {
-  entry: './src/index.ts',
+module.exports = {
+  entry: { main: './src/index.ts' },
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'messenger.bundle.js',
   },
+  devtool: 'source-map',
+  mode: 'development',
   devServer: {
-    open: true,
-    host: 'localhost',
-    port: 1234,
+    static: path.resolve(__dirname, './dist'),
     historyApiFallback: true,
+    compress: true,
+    port: 8080,
+    open: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    fallback: {
+      fs: false,
+    },
+    alias: {
+      core: path.resolve(__dirname, 'src/core'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      components: path.resolve(__dirname, 'src/components'),
+      img: path.resolve(__dirname, 'src/image'),
+      routes: path.resolve(__dirname, 'src/routes'),
+      types: path.resolve(__dirname, 'src/types'),
+      vendor: path.resolve(__dirname, 'src/vendor'),
+      variables: path.resolve(__dirname, 'src/variables'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      styles: path.resolve(__dirname, 'src/styles'),
+      api: path.resolve(__dirname, 'src/Api'),
+      services: path.resolve(__dirname, 'src/services'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.hbs$/,
+        use: ['handlebars-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
-
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: 'ts-loader',
-        exclude: ['/node_modules/'],
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          stylesHandler,
-          'css-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-      {
-        test: /\.hbs$/,
-        use: [
-          {
-            loader: 'handlebars-loader',
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.hbs', '.json'],
-  },
-};
-
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-  } else {
-    config.mode = 'development';
-  }
-  return config;
 };
